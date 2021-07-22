4769700f04a7c923e453cb081b158b54e3a08f76
csbaxter <drewjbaxter@gmail.com>
2021-07-22 15:57:34 -0400

    Update __init__.py

--- tutorials/linear/__init__.py
+++ tutorials/linear/__init__.py
@@ -14,10 +14,10 @@ def compiles():
 @check50.check(compiles)
 def score_50():
     """returns false for 50"""
-    check50.run("./affine").stdin("50").stdout("Sorry better luck nex time!\n").exit(0)
+    check50.run("./linear").stdin("50").stdout("Sorry better luck nex time!\n").exit(0)
     
 
 @check50.check(compiles)
 def score_14():
     """returns true for 14"""
-    check50.run("./affine").stdin("14").stdout("Found your number! Bingo!\n").exit(0)
+    check50.run("./linear").stdin("14").stdout("Found your number! Bingo!\n").exit(0)
