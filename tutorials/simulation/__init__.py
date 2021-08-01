3caa4f8ed2a77f777df198b2f375c1e842ab436e
csbaxter <drewjbaxter@gmail.com>
2021-08-01 19:11:21 -0400

    Update __init__.py

--- tutorials/simulation/__init__.py
+++ tutorials/simulation/__init__.py
@@ -3,12 +3,12 @@ import check50.c
 
 @check50.check()
 def exists():
-    """linear.c exists."""
+    """simulate.c and monte_hall.c exist."""
     check50.exists("simulate.c")
     check50.exists("monte_hall.c")
 
 @check50.check(exists)
 def compiles():
-    """linear.c compiles."""
+    """simulate.c and monte_hall.c compiles."""
     check50.c.compile("simulate.c", lcs50=True)
     check50.c.compile("monte_hall.c", lcs50=True)
